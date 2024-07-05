7ac4da8a938806b0a3365c63a9484b349afbf011
bugrahankaramollaoglu <bugrahankaramollaoglu@gmail.com>
2024-07-05 15:39:54 +0300

    ok. will try forgotPassword mechanism

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:ui';
 
 import 'package:auth_button_kit/auth_button_kit.dart';
@@ -6,12 +7,11 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:greefin/firebase/auth.dart';
-import 'package:greefin/onboarding.dart';
 import 'package:greefin/pages/home_page.dart';
 import 'package:neumorphic_button/neumorphic_button.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -19,6 +19,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String? errorMessage;
+  bool showLoginPage = true; // Control state for page transition
 
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -66,14 +67,26 @@ class _LoginPageState extends State<LoginPage> {
     bool isPassword,
   ) {
     return TextField(
+      style: TextStyle(color: Colors.black54),
+      cursorColor: Colors.black54,
       controller: controller,
       obscureText: isPassword,
       decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.3),
         prefixIcon: Icon(icon),
+        hintText: 'Enter',
+        hintStyle: TextStyle(color: Colors.grey),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black), // Unfocused border color
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black), // Focused border color
+        ),
         labelText: title,
         border: OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.black),
-          borderRadius: BorderRadius.circular(5),
+          borderSide: const BorderSide(color: Colors.white),
+          borderRadius: BorderRadius.circular(15),
         ),
       ),
     );
@@ -95,10 +108,10 @@ class _LoginPageState extends State<LoginPage> {
       onPressed: (method) => _signInWithEmailAndPassword(),
       brand: Method.custom,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5),
+        borderRadius: BorderRadius.circular(45),
       ),
-      customImage: Image.asset('assets/signin.png'),
       text: 'Login',
+      customImage: Image.asset('assets/signin.png'),
       backgroundColor: Colors.green,
       textColor: Colors.black54,
     );
@@ -106,15 +119,28 @@ class _LoginPageState extends State<LoginPage> {
 
   Widget _registerButton() {
     return AuthButton(
+      onPressed: (method) => setState(() => showLoginPage = false),
+      brand: Method.custom,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(45),
+      ),
+      text: 'Register',
+      customImage: Image.asset('assets/signup.png'),
+      backgroundColor: const Color.fromARGB(255, 200, 200, 31),
+      textColor: Colors.black54,
+    );
+  }
+
+  Widget _registerUserButton() {
+    return AuthButton(
       onPressed: (method) => _createUserWithEmailAndPassword(),
       brand: Method.custom,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5),
+        borderRadius: BorderRadius.circular(45),
       ),
-      customImage: Image.asset('assets/signup.png'),
-      text: 'Register',
-      backgroundColor: Color.fromARGB(255, 200, 200, 31),
-      textColor: Colors.black54,
+      text: 'Create User',
+      backgroundColor: Colors.white,
+      textColor: Colors.black,
     );
   }
 
@@ -126,7 +152,7 @@ class _LoginPageState extends State<LoginPage> {
         (route) => false,
       ),
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5),
+        borderRadius: BorderRadius.circular(45),
       ),
       brand: Method.custom,
       text: 'Continue as Guest',
@@ -167,21 +193,82 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+  // Forgot Password Functionality
   Widget _forgotPassword() {
     return Align(
       alignment: Alignment.centerLeft,
       child: TextButton(
-        onPressed: () {
-          // Add forgot password functionality here
-        },
+        onPressed: () => _showForgotPasswordDialog(context),
+        // Use a method to show the dialog
         child: Text(
           'Forgot Password?',
           style: TextStyle(
-            color: Colors.black.withOpacity(0.6),
+            color: Colors.black.withOpacity(0.7),
             fontSize: 16,
           ),
         ),
       ),
+    );
+  }
+
+  void _showForgotPasswordDialog(BuildContext context) {
+    TextEditingController _emailController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Forgot Password?'),
+          content: TextField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              labelText: 'Enter your email',
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Reset Password'),
+              onPressed: () async {
+                Navigator.pop(context); // Close the dialog
+                try {
+                  await FirebaseAuth.instance.sendPasswordResetEmail(
+                    email: _emailController.text.trim(),
+                  );
+
+                  // Check if the context is still valid before showing SnackBar
+                  if (ScaffoldMessenger.of(context).mounted) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Password reset email sent.'),
+                        duration: Duration(seconds: 5),
+                      ),
+                    );
+                  }
+                } catch (e) {
+                  print('Error sending password reset email: $e');
+                  // Check if the context is still valid before showing SnackBar
+                  if (ScaffoldMessenger.of(context).mounted) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          'Failed to send reset email. Please try again later.',
+                        ),
+                        duration: Duration(seconds: 5),
+                      ),
+                    );
+                  }
+                }
+              },
+            ),
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
@@ -234,6 +321,19 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
+  Widget _backToLoginButton() {
+    return AuthButton(
+      onPressed: (method) => setState(() => showLoginPage = true),
+      brand: Method.custom,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(45),
+      ),
+      text: 'Back to Login',
+      backgroundColor: Colors.black54,
+      textColor: Colors.white,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -247,25 +347,6 @@ class _LoginPageState extends State<LoginPage> {
               fit: BoxFit.cover,
             ),
           ),
-          /*  Positioned.fill(
-            child: Container(
-              color: Colors.brown,
-            ),
-          ), */
-          /* Positioned.fill(
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.black.withOpacity(0.1),
-                    Colors.black.withOpacity(0.5)
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-              ),
-            ),
-          ), */
           SingleChildScrollView(
             child: Container(
               width: double.maxFinite,
@@ -277,7 +358,7 @@ class _LoginPageState extends State<LoginPage> {
                   filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
                   child: Container(
                     height: 700,
-                    width: 350,
+                    width: 375,
                     decoration: BoxDecoration(
                       gradient: LinearGradient(
                         colors: [Colors.white60, Colors.white10],
@@ -292,31 +373,76 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     child: Padding(
                       padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            'GREEFIN',
-                            style: TextStyle(
-                                fontSize: 30, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 20),
-                          _entryField(
-                              'Email', _emailController, Icons.email, false),
-                          const SizedBox(height: 20),
-                          _entryField('Password', _passwordController,
-                              Icons.lock, true),
-                          _forgotPassword(),
-                          errorMessageWidget(),
-                          const SizedBox(height: 20),
-                          _loginButton(),
-                          _registerButton(),
-                          _guestButton(),
-                          const SizedBox(height: 30),
-                          _divider(),
-                          const SizedBox(height: 10),
-                          _oauthRow(),
-                        ],
+                      child: AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 500),
+                        transitionBuilder:
+                            (Widget child, Animation<double> animation) {
+                          return FadeTransition(
+                            opacity: animation,
+                            child: child,
+                          );
+                        },
+                        child: showLoginPage
+                            ? Column(
+                                key: const ValueKey('loginPage'),
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Text(
+                                    'GREEFIN',
+                                    style: TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 20),
+                                  _entryField(
+                                    'Email',
+                                    _emailController,
+                                    Icons.email,
+                                    false,
+                                  ),
+                                  const SizedBox(height: 20),
+                                  _entryField(
+                                    'Password',
+                                    _passwordController,
+                                    Icons.lock,
+                                    true,
+                                  ),
+                                  _forgotPassword(), // Forgot password button
+                                  errorMessageWidget(),
+                                  const SizedBox(height: 20),
+                                  _loginButton(),
+                                  _registerButton(),
+                                  _guestButton(),
+                                  const SizedBox(height: 30),
+                                  _divider(),
+                                  const SizedBox(height: 10),
+                                  _oauthRow(),
+                                ],
+                              )
+                            : Column(
+                                key: const ValueKey('registerPage'),
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  _entryField(
+                                    'Email',
+                                    _emailController,
+                                    Icons.email,
+                                    false,
+                                  ),
+                                  const SizedBox(height: 20),
+                                  _entryField(
+                                    'Password',
+                                    _passwordController,
+                                    Icons.lock,
+                                    true,
+                                  ),
+                                  errorMessageWidget(),
+                                  const SizedBox(height: 20),
+                                  _registerUserButton(),
+                                  _backToLoginButton(),
+                                ],
+                              ),
                       ),
                     ),
                   ),
