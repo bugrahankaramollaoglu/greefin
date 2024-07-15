e7868bc85df1af5b2565614b409a449fe49eb6e3
bugrahankaramollaoglu <bugrahankaramollaoglu@gmail.com>
2024-07-15 23:27:00 +0300

    still working on manual adding. will continue tomorrow

--- lib/model/purchase.dart
+++ lib/model/purchase.dart
@@ -2,11 +2,13 @@ class Purchase {
   final String id;
   final String name;
   final DateTime? date;
+  final String category;
   final double price;
 
   Purchase(
       {required this.id,
       required this.name,
       required this.date,
+      required this.category,
       required this.price});
 }
