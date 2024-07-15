73261552839fea04664401d04c6a3ffdc41cb5b3
bugrahankaramollaoglu <bugrahankaramollaoglu@gmail.com>
2024-07-15 21:57:05 +0300

    ok. will try to add mvvm

---
+++ lib/model/purchase.dart
@@ -0,0 +1,12 @@
+class Purchase {
+  final String id;
+  final String name;
+  final DateTime date;
+  final double price;
+
+  Purchase(
+      {required this.id,
+      required this.name,
+      required this.date,
+      required this.price});
+}
